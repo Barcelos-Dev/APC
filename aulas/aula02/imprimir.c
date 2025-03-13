ff9f41358950da5e980051471663adec5da81afa
Carlos <carlosbarcelos24@gmail.com>
2025-03-12 21:49:41 -0300

    codigos da aula 2

---
+++ aulas/aula02/imprimir.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main () {
+    printf("Boa noite\n\n");
+    printf("\tEstamos aprendendo C!");// /t é o espaço de tab 
+    return 0;
+}
---
+++ aulas/aula02/imprimir_caracter.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+
+    printf("%c\n", 'A');//' simples é caracter
+    printf("Ola %s\n", "Jose");// " duplas é texto/string
+    return 0;
+}
