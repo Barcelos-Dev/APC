8049893ebe4911d121e1c82fd7ffb17e9deda0b2
Carlos <carlosbarcelos24@gmail.com>
2025-03-19 20:28:32 -0300

    Atualização no nota legal

--- praticas/pratica02/imprima_nota_legal.c
+++ praticas/pratica02/imprima_nota_legal.c
@@ -2,14 +2,14 @@
 
 int main (){
 
-    printf("%s\n","==============================");
+    printf("%s\n","===============================");
     printf("%20s\n","Nota Legal");
-    printf("%s\n","==============================");
-    printf("%s\t\t %s %s\n","Produto","Qtd","Valor Unit");
-    printf("%s\t %03i\t %.2f\n","Camiseta", 2, 39.99);
-    printf("%s\t\t %03i\t %.2f\n","Calca", 1, 89.90);
-    printf("%s\t %03i\t %.2f\n","Meia Social", 3, 19.99);
-    printf("%s","==============================");
+    printf("%s\n","===============================");
+    printf("%-13s %6s %10s\n","Produto","Qtd","Valor Unit");
+    printf("%-13s %6.03i %10.2f\n","Camiseta", 2, 39.99);
+    printf("%-13s %6.03i %10.2f\n","Calca", 1, 89.90);
+    printf("%-13s %6.03i %10.2f\n","Meia Social", 3, 19.99);
+    printf("%s","===============================");
 
     return 0;
 }
